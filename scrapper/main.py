7632b183ef1fc626edd982b9825995ff80230497
Ivan Nedosekov <ivan-nedd@mail.ru>
2022-11-20 18:31:54 +0300

    split to files

--- scrapper/main.py
+++ scrapper/main.py
@@ -1,68 +1,12 @@
 import asyncio
-from collections import deque
-from itertools import chain, islice
+from itertools import chain
 from time import time
-from typing import Iterable, TypeVar
 
 from aiolimiter import AsyncLimiter
-from bs4 import BeautifulSoup
-from httpx import AsyncClient, Response, Timeout, codes
+from httpx import AsyncClient, Timeout
 from loguru import logger
 
-T = TypeVar('T')
-
-class ChankedQueue(deque[tuple[T, ...]]):
-    def __init__(self, chunk_size: int = 50, *arg, **kwargs) -> None:
-        self.chunk_size = chunk_size
-        super(ChankedQueue, self).__init__(*arg, **kwargs)
-
-    def extend(self, iterable: Iterable[T]) -> None:  # type: ignore
-        real_dequeue = super(ChankedQueue, self)
-        start = 0
-        if real_dequeue.__bool__():
-            last_chunk = real_dequeue.pop()
-            stop = self.chunk_size - len(last_chunk)
-        else:
-            last_chunk = tuple()
-            stop = self.chunk_size
-
-        while True:
-            next_chunk = tuple(chain(last_chunk, islice(iterable, start, stop)))
-            if not next_chunk:
-                break
-
-            real_dequeue.append(next_chunk)
-            last_chunk = tuple()
-            start, stop = stop, stop + self.chunk_size
-
-
-def get_all_url_from_html(source: str) -> set[str]:
-    soup = BeautifulSoup(source, features="html.parser")
-    return {a['href'] for a in soup.find_all('a', href=True)}
-
-async def make_request(url: str, session: AsyncClient, throttler: AsyncLimiter) -> Response:
-    async with throttler:
-        logger.debug('scrapy {}', url)
-        return await session.get(url)
-
-
-async def srape(url: str, session: AsyncClient, throttler: AsyncLimiter) -> Response | None:
-    response = await make_request(url, session, throttler)
-
-    if response.status_code == codes.OK:
-        return response
-
-    if response.status_code == codes.TOO_MANY_REQUESTS or response.status_code == codes.SERVICE_UNAVAILABLE:
-        logger.warning('retry later by {} on url {}', response.status_code, url)
-        await asyncio.sleep(1.5)
-        response = await make_request(url, session, throttler)
-
-    if response.status_code != codes.OK:
-        logger.error('cannot scrape {} code {}', url, response.status_code)
-        return None
-
-    return response
-
+from .scrapy import ChankedQueue, get_all_url_from_html, scrape
 
 URL = 'http://neolurk.org'
 MAIN_PAGE = '/wiki/Заглавная_страница'
@@ -86,7 +30,7 @@ async def run():
     async with AsyncClient(timeout=Timeout(10, connect=60)) as session:
         while len(visited) < 50:
             url_chunk = url_to_process.popleft()
-            tasks = [srape(url, session=session, throttler=throttler) for url in url_chunk]
+            tasks = [scrape(url, session=session, throttler=throttler) for url in url_chunk]
             results = await asyncio.gather(*tasks)
             visited.update(url_chunk)
             url_in_results = set(
---
+++ scrapper/scrapy.py
@@ -0,0 +1,65 @@
+import asyncio
+from collections import deque
+from itertools import chain, islice
+from typing import TypeVar, Iterable
+
+from aiolimiter import AsyncLimiter
+from bs4 import BeautifulSoup
+from httpx import AsyncClient, Response, codes
+from loguru import logger
+
+T = TypeVar('T')
+
+
+class ChankedQueue(deque[tuple[T, ...]]):
+    def __init__(self, chunk_size: int = 50, *arg, **kwargs) -> None:
+        self.chunk_size = chunk_size
+        super(ChankedQueue, self).__init__(*arg, **kwargs)
+
+    def extend(self, iterable: Iterable[T]) -> None:  # type: ignore
+        real_dequeue = super(ChankedQueue, self)
+        start = 0
+        if real_dequeue.__bool__():
+            last_chunk = real_dequeue.pop()
+            stop = self.chunk_size - len(last_chunk)
+        else:
+            last_chunk = tuple()
+            stop = self.chunk_size
+
+        while True:
+            next_chunk = tuple(chain(last_chunk, islice(iterable, start, stop)))
+            if not next_chunk:
+                break
+
+            real_dequeue.append(next_chunk)
+            last_chunk = tuple()
+            start, stop = stop, stop + self.chunk_size
+
+
+def get_all_url_from_html(source: str) -> set[str]:
+    soup = BeautifulSoup(source, features="html.parser")
+    return {a['href'] for a in soup.find_all('a', href=True)}
+
+
+async def make_request(url: str, session: AsyncClient, throttler: AsyncLimiter) -> Response:
+    async with throttler:
+        logger.debug('scrapy {}', url)
+        return await session.get(url)
+
+
+async def scrape(url: str, session: AsyncClient, throttler: AsyncLimiter) -> Response | None:
+    response = await make_request(url, session, throttler)
+
+    if response.status_code == codes.OK:
+        return response
+
+    if response.status_code == codes.TOO_MANY_REQUESTS or response.status_code == codes.SERVICE_UNAVAILABLE:
+        logger.warning('retry later by {} on url {}', response.status_code, url)
+        await asyncio.sleep(1.5)
+        response = await make_request(url, session, throttler)
+
+    if response.status_code != codes.OK:
+        logger.error('cannot scrape {} code {}', url, response.status_code)
+        return None
+
+    return response
