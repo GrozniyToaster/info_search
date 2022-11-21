be905fcd85cd26525c84def836addd92fa7fd4b8
Ivan Nedosekov <ivan-nedd@mail.ru>
2022-11-21 23:32:08 +0300

    add clear lexems

--- scrapper/documents.py
+++ scrapper/documents.py
@@ -2,6 +2,51 @@ from bs4 import BeautifulSoup
 from httpx import Response
 from loguru import logger
 from motor.motor_asyncio import AsyncIOMotorClient
+from pymystem3 import Mystem
+
+system = Mystem()
+part_of_speech = {
+    'A',  # прилагательное
+    'ADV',  # наречие
+    'ADVPRO',  # местоименное наречие
+    'ANUM',  # числительное-прилагательное
+    'COM',  # часть композита - сложного слова
+    'NUM',  # числительное
+    'S',  # существительное
+    'V',  # глагол
+
+    # 'APRO'	    # местоимение-прилагательное
+    # 'CONJ'	    # союз
+    # 'INTJ'	    # междометие
+    # 'PART'	    # частица
+    # 'PR'	    # предлог
+    # 'SPRO'	    # местоимение-существительное
+}
+
+
+def get_clear_lemmas(text: str) -> list[dict]:
+    analyzed_symbols: list[dict] = system.analyze(text)
+    return [
+        symbol
+        for symbol in analyzed_symbols
+        if is_significant_token(symbol)
+    ]
+
+
+
+
+def is_significant_grammar(grammar: str) -> bool:
+    name = grammar.split('=')[0].split(',')[0]
+    return name in part_of_speech
+
+
+def is_significant_token(token: dict) -> bool:
+    match token:
+        case {'analysis': [{'gr': str(grammar)}]} if is_significant_grammar(grammar):
+            return True
+        case _:
+            return False
+
 
 client = AsyncIOMotorClient()
 
@@ -15,7 +60,7 @@ async def upload_documents(responses: list[Response | None]) -> None:
             {
                 'path': response.url.path,
                 'raw_html': response.text,
-                'text':  BeautifulSoup(response.text).get_text()
+                'lemmas': get_clear_lemmas(BeautifulSoup(response.text).get_text())
             }
             for response in responses if response
         ]
